72dcc01066aa00d442589b842a38259bee2a90db
kenkelbt <kenkelbt@mail.uc.edu>
2023-04-08 15:43:16 -0400

    Added more UI components and study section

--- Smart Learn/Form1.Designer.cs
+++ Smart Learn/Form1.Designer.cs
@@ -33,9 +33,18 @@
             txtTest = new TextBox();
             pnlTest = new Panel();
             pnlMain = new Panel();
+            btnStudy = new Button();
             lstSets = new ListBox();
+            pnlStudy = new Panel();
+            btnAnswer = new Button();
+            txtAnswer = new TextBox();
+            rtxtDef = new RichTextBox();
+            pnlCorI = new Panel();
+            lblCorI = new Label();
             pnlTest.SuspendLayout();
             pnlMain.SuspendLayout();
+            pnlStudy.SuspendLayout();
+            pnlCorI.SuspendLayout();
             SuspendLayout();
             // 
             // btnTest
@@ -79,11 +88,22 @@
             // pnlMain
             // 
             pnlMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlMain.Controls.Add(btnStudy);
             pnlMain.Controls.Add(lstSets);
             pnlMain.Location = new Point(0, 0);
             pnlMain.Name = "pnlMain";
             pnlMain.Size = new Size(799, 449);
             pnlMain.TabIndex = 3;
+            // 
+            // btnStudy
+            // 
+            btnStudy.Location = new Point(12, 12);
+            btnStudy.Name = "btnStudy";
+            btnStudy.Size = new Size(105, 39);
+            btnStudy.TabIndex = 1;
+            btnStudy.Text = "Study";
+            btnStudy.UseVisualStyleBackColor = true;
+            btnStudy.Click += btnStudy_Click;
             // 
             // lstSets
             // 
@@ -94,11 +114,69 @@
             lstSets.Size = new Size(776, 379);
             lstSets.TabIndex = 0;
             // 
+            // pnlStudy
+            // 
+            pnlStudy.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlStudy.Controls.Add(btnAnswer);
+            pnlStudy.Controls.Add(txtAnswer);
+            pnlStudy.Controls.Add(rtxtDef);
+            pnlStudy.Location = new Point(0, 0);
+            pnlStudy.Name = "pnlStudy";
+            pnlStudy.Size = new Size(799, 449);
+            pnlStudy.TabIndex = 2;
+            // 
+            // btnAnswer
+            // 
+            btnAnswer.Location = new Point(692, 404);
+            btnAnswer.Name = "btnAnswer";
+            btnAnswer.Size = new Size(96, 34);
+            btnAnswer.TabIndex = 3;
+            btnAnswer.Text = "Answer";
+            btnAnswer.UseVisualStyleBackColor = true;
+            btnAnswer.Click += btnAnswer_Click;
+            // 
+            // txtAnswer
+            // 
+            txtAnswer.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            txtAnswer.Location = new Point(12, 404);
+            txtAnswer.Name = "txtAnswer";
+            txtAnswer.Size = new Size(674, 34);
+            txtAnswer.TabIndex = 2;
+            // 
+            // rtxtDef
+            // 
+            rtxtDef.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            rtxtDef.Location = new Point(12, 12);
+            rtxtDef.Name = "rtxtDef";
+            rtxtDef.Size = new Size(776, 386);
+            rtxtDef.TabIndex = 1;
+            rtxtDef.Text = "";
+            // 
+            // pnlCorI
+            // 
+            pnlCorI.Controls.Add(lblCorI);
+            pnlCorI.Location = new Point(0, 0);
+            pnlCorI.Name = "pnlCorI";
+            pnlCorI.Size = new Size(799, 449);
+            pnlCorI.TabIndex = 4;
+            // 
+            // lblCorI
+            // 
+            lblCorI.AutoSize = true;
+            lblCorI.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCorI.Location = new Point(4, 11);
+            lblCorI.Name = "lblCorI";
+            lblCorI.Size = new Size(105, 45);
+            lblCorI.TabIndex = 0;
+            lblCorI.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pnlCorI);
+            Controls.Add(pnlStudy);
             Controls.Add(pnlMain);
             Controls.Add(pnlTest);
             Name = "Form1";
@@ -106,6 +184,10 @@
             pnlTest.ResumeLayout(false);
             pnlTest.PerformLayout();
             pnlMain.ResumeLayout(false);
+            pnlStudy.ResumeLayout(false);
+            pnlStudy.PerformLayout();
+            pnlCorI.ResumeLayout(false);
+            pnlCorI.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -117,5 +199,12 @@
         private Panel pnlTest;
         private Panel pnlMain;
         private ListBox lstSets;
+        private Button btnStudy;
+        private Panel pnlStudy;
+        private Button btnAnswer;
+        private TextBox txtAnswer;
+        private RichTextBox rtxtDef;
+        private Panel pnlCorI;
+        private Label lblCorI;
     }
 }
--- Smart Learn/Form1.cs
+++ Smart Learn/Form1.cs
@@ -10,34 +10,82 @@ namespace SmartLearn
         private readonly string tadFilePath = @".\TermsAndDefinitions\";
         List<TermsAndDefinitions> tad = new List<TermsAndDefinitions>();
         string[] setNames;
+        string selectedItem = "";
 
         public Form1()
         {
             InitializeComponent();
+            DisablePanels();
+            pnlMain.Visible = true;
+
+            // Get set names
             setNames = Directory.GetFiles(tadFilePath, "*", SearchOption.AllDirectories).Select(x => Path.GetFileNameWithoutExtension(x)).ToArray();
             lstSets.DataSource = setNames;
         }
 
+        // Disable all panels
+        private void DisablePanels()
+        {
+            pnlMain.Visible = false;
+            pnlStudy.Visible = false;
+            pnlTest.Visible = false;
+        }
+
         private void btnTest_Click(object sender, EventArgs e)
         {
+            // Add example term and definition
             tad.Add(new TermsAndDefinitions()
             {
                 term = "Test Term",
                 definition = "Test Definition"
             });
 
-            string json = System.Text.Json.JsonSerializer.Serialize(tad);
-            File.WriteAllText(tadFilePath + "TestName.json", json);
+            WriteJSON();
         }
 
         private void btnRead_Click(object sender, EventArgs e)
         {
-            using StreamReader reader = new(tadFilePath + "TestName.json");
-            var json = reader.ReadToEnd();
-            List<TermsAndDefinitions>? tadRead = JsonConvert.DeserializeObject<List<TermsAndDefinitions>>(json);
+            // Open stream reader and deserialize/read json file
+            ReadJSON();
 
-            txtTest.Text = tadRead[0].term + "|" + tadRead[0].definition;
+            txtTest.Text = tad[0].term + "|" + tad[0].definition;
         }
 
+        private void btnStudy_Click(object sender, EventArgs e)
+        {
+            if (lstSets.Items.Count > 0)
+            {
+                selectedItem = lstSets.SelectedItem.ToString();
+            }
+
+            DisablePanels();
+            pnlStudy.Visible = true;
+
+            ReadJSON();
+            rtxtDef.Text = tad[0].definition;
+        }
+
+        private void ReadJSON()
+        {
+            // Open stream reader and deserialize/read json file
+            using StreamReader reader = new(tadFilePath + "TestName.json");
+            var json = reader.ReadToEnd();
+            tad = JsonConvert.DeserializeObject<List<TermsAndDefinitions>>(json);
+        }
+
+        private void WriteJSON()
+        {
+            // Serialize json and write to file
+            string json = System.Text.Json.JsonSerializer.Serialize(tad);
+            File.WriteAllText(tadFilePath + "TestName.json", json);
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            if (txtAnswer.Text.ToLower() == tad[0].term.ToLower())
+            {
+
+            }
+        }
     }
 }
