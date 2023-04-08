8f7d8b4b519f25fbd88767ac59c1a1c4c7735477
kenkelbt <kenkelbt@mail.uc.edu>
2023-04-08 15:00:23 -0400

    Added list box that will display all set names

--- Smart Learn/Form1.Designer.cs
+++ Smart Learn/Form1.Designer.cs
@@ -31,11 +31,16 @@
             btnTest = new Button();
             btnRead = new Button();
             txtTest = new TextBox();
+            pnlTest = new Panel();
+            pnlMain = new Panel();
+            lstSets = new ListBox();
+            pnlTest.SuspendLayout();
+            pnlMain.SuspendLayout();
             SuspendLayout();
             // 
             // btnTest
             // 
-            btnTest.Location = new Point(0, 0);
+            btnTest.Location = new Point(4, 3);
             btnTest.Name = "btnTest";
             btnTest.Size = new Size(75, 23);
             btnTest.TabIndex = 0;
@@ -45,7 +50,7 @@
             // 
             // btnRead
             // 
-            btnRead.Location = new Point(0, 29);
+            btnRead.Location = new Point(4, 32);
             btnRead.Name = "btnRead";
             btnRead.Size = new Size(75, 23);
             btnRead.TabIndex = 1;
@@ -55,23 +60,53 @@
             // 
             // txtTest
             // 
-            txtTest.Location = new Point(85, 8);
+            txtTest.Location = new Point(85, 4);
             txtTest.Name = "txtTest";
             txtTest.Size = new Size(527, 23);
             txtTest.TabIndex = 2;
+            // 
+            // pnlTest
+            // 
+            pnlTest.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlTest.Controls.Add(txtTest);
+            pnlTest.Controls.Add(btnTest);
+            pnlTest.Controls.Add(btnRead);
+            pnlTest.Location = new Point(0, 0);
+            pnlTest.Name = "pnlTest";
+            pnlTest.Size = new Size(799, 449);
+            pnlTest.TabIndex = 3;
+            // 
+            // pnlMain
+            // 
+            pnlMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlMain.Controls.Add(lstSets);
+            pnlMain.Location = new Point(0, 0);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(799, 449);
+            pnlMain.TabIndex = 3;
+            // 
+            // lstSets
+            // 
+            lstSets.FormattingEnabled = true;
+            lstSets.ItemHeight = 15;
+            lstSets.Location = new Point(12, 57);
+            lstSets.Name = "lstSets";
+            lstSets.Size = new Size(776, 379);
+            lstSets.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(txtTest);
-            Controls.Add(btnRead);
-            Controls.Add(btnTest);
+            Controls.Add(pnlMain);
+            Controls.Add(pnlTest);
             Name = "Form1";
             Text = "Form1";
+            pnlTest.ResumeLayout(false);
+            pnlTest.PerformLayout();
+            pnlMain.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -79,5 +114,8 @@
         private Button btnTest;
         private Button btnRead;
         private TextBox txtTest;
+        private Panel pnlTest;
+        private Panel pnlMain;
+        private ListBox lstSets;
     }
 }
--- Smart Learn/Form1.cs
+++ Smart Learn/Form1.cs
@@ -7,33 +7,37 @@ namespace SmartLearn
 {
     public partial class Form1 : Form
     {
-        private readonly string tadFilePath = @".\TermsAndDefinitions\tad.json";
+        private readonly string tadFilePath = @".\TermsAndDefinitions\";
+        List<TermsAndDefinitions> tad = new List<TermsAndDefinitions>();
+        string[] setNames;
 
         public Form1()
         {
             InitializeComponent();
+            setNames = Directory.GetFiles(tadFilePath, "*", SearchOption.AllDirectories).Select(x => Path.GetFileNameWithoutExtension(x)).ToArray();
+            lstSets.DataSource = setNames;
         }
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            List<TermsAndDefinitions> tadCreate = new List<TermsAndDefinitions>();
-            tadCreate.Add(new TermsAndDefinitions()
+            tad.Add(new TermsAndDefinitions()
             {
                 term = "Test Term",
                 definition = "Test Definition"
             });
 
-            string json = System.Text.Json.JsonSerializer.Serialize(tadCreate);
-            File.WriteAllText(tadFilePath, json);
+            string json = System.Text.Json.JsonSerializer.Serialize(tad);
+            File.WriteAllText(tadFilePath + "TestName.json", json);
         }
 
         private void btnRead_Click(object sender, EventArgs e)
         {
-            using StreamReader reader = new(tadFilePath);
+            using StreamReader reader = new(tadFilePath + "TestName.json");
             var json = reader.ReadToEnd();
             List<TermsAndDefinitions>? tadRead = JsonConvert.DeserializeObject<List<TermsAndDefinitions>>(json);
 
             txtTest.Text = tadRead[0].term + "|" + tadRead[0].definition;
         }
+
     }
 }
